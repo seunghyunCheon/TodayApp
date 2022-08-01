a135fc5c8c69ae693cddf6fcce02a6d6acdc1ace
pololover <pololover>
2022-08-01 18:17:08 +0900

    FEAT: 초기 SnapShot 설정 / DataSource 등록

--- TodayApp/ReminderListViewController.swift
+++ TodayApp/ReminderListViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
 
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    // Diffable Data Source를 사용함으로써 테이블뷰에 데이터가 변화되는 것과 UI가 업데이트 되는 것을 동기화 시킬 수 있다.
+    // 즉 기존에는 동기화가 잘 되지 않아 크래시를 막기 위해 reloadData()를 사용했지만 이를 이용하면, apply메서드만으로 동기화가 가능해진다.
     
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
     var dataSource: DataSource!
     
     override func viewDidLoad() {
@@ -30,6 +33,13 @@ class ReminderListViewController: UICollectionViewController {
         dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         })
+        
+        // data state를 담는 초기 snapShot을 정의
+        var snapshot = SnapShot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Reminder.sampleData.map{ $0.title })
+        dataSource.apply(snapshot)
+        collectionView.dataSource = dataSource
     }
 
     
