fae60da3ad6b3daf0cd433be927bcc461605b6d7
pololover <pololover>
2022-08-01 17:59:41 +0900

    FEAT: Diffable Data Source 설정

--- TodayApp/ReminderListViewController.swift
+++ TodayApp/ReminderListViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
 
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+            
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
     }
 
     
