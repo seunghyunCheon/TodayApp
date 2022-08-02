c0884c7011674f4c63e0ba9929a7a8e7864b2776
pololover <pololover>
2022-08-02 12:06:17 +0900

    FEAT: 버튼에 상태 VoiceOver 접근성 기능 추가.

--- TodayApp/ListViewController/ReminderListViewController+DataSource.swift
+++ TodayApp/ListViewController/ReminderListViewController+DataSource.swift
@@ -7,6 +7,14 @@ extension ReminderListViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
     typealias SnapShot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
     
+    var reminderCompletedValue: String {
+        NSLocalizedString("Completed", comment: "Reminder completed value")
+    }
+    
+    var reminderNotCompletedValue: String {
+        NSLocalizedString("Not completed", comment: "Reminder not completed value")
+    }
+    
     // 셀의 데이터가 변경되었을 때마다 snapShot을 업데이트하고, apply시켜야 적용이 됨.
     func updateSnapShot(reloading ids: [Reminder.ID] = []) {
         var snapshot = SnapShot()
@@ -22,6 +30,8 @@ extension ReminderListViewController {
     func cellRegisterationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
         //이 부분에서 id에 따라 reminder를 설정한 것이 configuration이나 dondButtonConfiguration 등에 사용되기 때문에 indexPath가 아닌 id로 통일해야함.
         let reminder = reminder(for: id)
+        
+        // 내용에 대한 속성
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
@@ -30,13 +40,21 @@ extension ReminderListViewController {
         contentConfiguration.secondaryTextProperties.color = .white
         cell.contentConfiguration = contentConfiguration
         
+        // 버튼에 대한 속성
         var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
         doneButtonConfiguration.tintColor = .gray
+        cell.accessibilityCustomActions = [ doneButtonAccessibilityAction(for: reminder)]
+        cell.accessibilityValue = reminder.isComplete ? reminderCompletedValue : reminderNotCompletedValue
         cell.accessories = [ .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)]
         
+        
+        // 버튼 스타일에 대한 속성
+        cell.layer.borderColor = UIColor.white.cgColor
+        cell.layer.borderWidth = 1
         var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
-        backgroundConfiguration.backgroundColor = .darkGray
+        backgroundConfiguration.backgroundColor = .black
         cell.backgroundConfiguration = backgroundConfiguration
+        
     }
     
     func completeReminder(with id: Reminder.ID) {
@@ -44,6 +62,15 @@ extension ReminderListViewController {
         reminder.isComplete.toggle()
         update(reminder, with: id)
         updateSnapShot(reloading: [id])
+    }
+    
+    private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction {
+        let name = NSLocalizedString("Toggle completion", comment: "Reminder done button accessibility label")
+        let action =  UIAccessibilityCustomAction(name: name) { [weak self] action in
+            self?.completeReminder(with: reminder.id)
+            return true
+        }
+        return action
     }
     
     private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
